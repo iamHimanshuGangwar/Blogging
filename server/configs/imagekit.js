73b74c7c70b5a071eb0a76f9ecbf39c0f4fcf75c
Himanshu Gangwar <himanshugangwar59943@gmail.com>
2026-03-15 11:37:00 +0530

    Update imagekit.js

--- server/configs/imagekit.js
+++ server/configs/imagekit.js
@@ -8,7 +8,6 @@ const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint:process.env.IMAGEKIT_URL_ENDPOINT,
-
 });
 export default imagekit;
 // SDK initialization
