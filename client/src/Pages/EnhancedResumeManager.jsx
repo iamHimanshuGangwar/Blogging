487426829e996351835a10dfa642e75a269ac1e8
Himanshu Gangwar <himanshugangwar59943@gmail.com>
2026-03-29 19:15:23 +0530

    Update EnhancedResumeManager.jsx

--- client/src/Pages/EnhancedResumeManager.jsx
+++ client/src/Pages/EnhancedResumeManager.jsx
@@ -397,6 +397,7 @@ const EnhancedResumeManager = () => {
                   onChange={(e) =>
                     setNewResume({ ...newResume, name: e.target.value })
                   }
+                  
                   placeholder="e.g., Senior React Developer"
                   className="w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
